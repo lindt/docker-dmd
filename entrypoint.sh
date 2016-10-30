e5d0b846e86cfa20fd6d0a9e9489b0b05ffc6136
stefan <think@hotmail.de>
2016-10-30 03:12:17 +0100

    cleaned up shell scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ USER_ID=${LOCAL_USER_ID:-0}
 GROUP_ID=${LOCAL_GROUP_ID:-0}
 CMD=${*:-/bin/bash}
 
-if [ "$USER_ID" -ne "0" ]; then
-  groupadd -g $GROUP_ID $USER_NAME &> /dev/null
-  useradd --shell /bin/bash -M -u $USER_ID -g $GROUP_ID -o -c "" $USER_NAME
+if [ "${USER_ID}" -ne "0" ]; then
+  groupadd -g ${GROUP_ID} ${USER_NAME} &> /dev/null
+  useradd --shell /bin/bash -M -u ${USER_ID} -g ${GROUP_ID} -o -c "" ${USER_NAME}
   usermod -aG sudo ${USER_NAME}
 
-  /usr/local/bin/gosu $USER_NAME /bin/bash -c "$CMD"
+  /usr/local/bin/gosu ${USER_NAME} /bin/bash -c "${CMD}"
 else
-  /bin/bash -c "$CMD"
+  /bin/bash -c "${CMD}"
 fi
