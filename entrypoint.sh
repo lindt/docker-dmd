e55d5476e5b02ae85de5c22019305fd40d024781
lindt <think@hotmail.de>
2018-04-15 18:41:30 +0200

    removed default CMD

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,13 @@
 USER_NAME=${USER:-root}
 USER_ID=${LOCAL_USER_ID:-0}
 GROUP_ID=${LOCAL_GROUP_ID:-0}
-CMD=${*:-/bin/bash}
 
 if [ "${USER_ID}" -ne "0" ]; then
   groupadd -g ${GROUP_ID} ${USER_NAME} &> /dev/null
   useradd --shell /bin/bash -M -u ${USER_ID} -g ${GROUP_ID} -o -c "" ${USER_NAME}
   usermod -aG sudo ${USER_NAME}
 
-  /usr/local/bin/gosu ${USER_NAME} /bin/bash -c "${CMD}"
+  /usr/local/bin/gosu ${USER_NAME} /bin/bash -c "$@"
 else
-  /bin/bash -c "${CMD}"
+  /bin/bash -c "$@"
 fi
